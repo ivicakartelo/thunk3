2fef17f5f990907e72f78c86ac095de565ee46ed
ivicakartelo <ivicakartelo@users.noreply.github.com>
2023-09-08 17:06:50 +0200

    template/CardTailwindResponsive.js

--- src/template/CardTailwindResponsive.js
+++ src/template/CardTailwindResponsive.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export default function CardTailwindResponsive() {
   return (
-<div className="grid sm:grid-cols-2 md:grid-cols-3">
+<div className="grid sm:grid-cols-2 md:grid-cols-4">
   <p>Column</p>
   <p>Column</p>
   <p>Column</p>
